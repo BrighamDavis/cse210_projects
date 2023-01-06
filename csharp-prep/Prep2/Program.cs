1410a4ca2a7465535d2aedcc712f784df64ae239
Brigham Davis <brighamjdavis@gmail.com>
2023-01-05 19:39:10 -0800

    prep 01 and 02

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string val;
+        int grade;
+        
+        Console.Write("What is the grade percentge?: ");
+        val = Console.ReadLine();
+
+        // convert to integer
+        grade = Convert.ToInt32(val);
+        
+        if (grade >=  90)
+        {
+            Console.WriteLine("The grade is an 'A'");
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            Console.WriteLine("The grade is a 'B'");
+        }
+        else if (grade < 80 && grade >= 70)
+        {
+            Console.WriteLine("The grade is a 'C'");
+        }
+        else if (grade < 70 && grade >= 60)
+        {
+            Console.WriteLine("The grade is a 'D'");
+        }
+        else
+        {
+            Console.WriteLine("The grade is an 'F'");
+        }
     }
 }
