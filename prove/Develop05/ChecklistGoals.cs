e2f888c615d4f1ea320dd2ba65d0fdf228f4f29a
BrighamDavis <brighamjdavis@gmail.com>
2023-03-25 21:32:29 -0700

    Foundation 4 - 1

---
+++ prove/Develop05/ChecklistGoals.cs
@@ -0,0 +1,15 @@
+public class Checklist : Goal
+{
+    public override void Display()
+    {
+        setGoalName();
+        setGoalDescription();
+        setGoalPoints();
+        setGoalRepeat();
+        setRepeatPoints();
+    }
+    public override string GetStringRepresentation()
+    {
+        return $"{getGoalType()}:{GetGoalName()},{GetGoalDescription()},{GetGoalPoints()},{GetRepeatBonus()},{GetRepeatPoints()},{getcompletedRepeat()}";
+    }
+}
